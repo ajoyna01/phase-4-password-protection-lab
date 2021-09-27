c1ad458a6f3440cc90f6e6320fc912ca71b289b7
Anna Wilson <ajoyna01@gmail.com>
2021-09-27 15:32:14 -0500

    commit tree d4ca054bc3460741558569fece231d6bdcae0d2c

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
